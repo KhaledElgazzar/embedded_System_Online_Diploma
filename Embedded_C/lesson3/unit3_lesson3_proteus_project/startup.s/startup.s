1b2c1dc6455b64d5b45fc8825f28b5b1143ca270
khaled elgazzar <khaled.elgazzzar@gmail.com>
2024-04-24 07:32:08 +0200

    adding makefile - startup.c - startup.s - proteus project

---
+++ Embedded_C/lesson3/unit3_lesson3_proteus_project/startup.s/startup.s
@@ -0,0 +1,31 @@
+.section .vectors
+.word  0x20001000           
+.word   _reset              
+.word 	 vector_handler       
+.word 	 vector_handler
+.word 	 vector_handler
+.word 	 vector_handler
+.word 	 vector_handler
+.word 	 vector_handler
+.word 	 vector_handler
+.word 	 vector_handler
+.word 	 vector_handler
+.word 	 vector_handler
+.word 	 vector_handler
+.word 	 vector_handler
+.word 	 vector_handler
+.word 	 vector_handler
+.word 	 vector_handler
+.word 	 vector_handler
+.word 	 vector_handler
+.word 	 vector_handler      
+
+
+.section .text
+
+_reset:
+	bl main
+	b .
+vector_handler:
+
+		b  _reset
